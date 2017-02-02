cfb9629bf01b98f14fa4e44eaed4b29f14475e5d
DJviolin <kerozin.joe@gmail.com>
2017-02-02 23:03:19 +0100

    2017. 02. 02._23:03:19,14

--- app/routes/index.js
+++ app/routes/index.js
@@ -40,7 +40,7 @@ router.get('/awesome', async (ctx) => {
 
 	ctx.body = await index({
 		welcome: `
-			<span style="color: #f00;">${json.name}<span>!
+			<span style="color: #f00;">${json.name}</span>!
 		`,
 		num: 2,
 	}, {
