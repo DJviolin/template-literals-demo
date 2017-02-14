289a92b76f8e112587e29ea4073f5b384575b458
DJviolin <kerozin.joe@gmail.com>
2017-02-14 11:53:55 +0100

    2017. 02. 14._11:53:54,89

--- app/views/components/loop.js
+++ app/views/components/loop.js
@@ -1,28 +1,10 @@
 'use strict';
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
-// https://rainsoft.io/how-three-dots-changed-javascript/
-
-// Proper module handling with type checking
-// https://gist.github.com/AoJ/547bc678347c18a4051c#file-fibonacci-js
-
-/*
-${Array(5).join(0).split(0).map((item, i) => `
-  <div>I am item number ${i}.</div>
-`).join('')}
-*/
-
-//console.time('benchmark');
-
-//const loop = (elem, obj) => {
 const loop = (elem1, obj, elem2) => {
   //console.log(args);
   let results = '';
-  //const split = elem.split(/<<>>/); // '<li>Number <<>></li>'
-  //console.log(split);
   try {
     for (let i = 0; i < obj.length; i += 1) {
-      //results += `${split[0]}${obj[i]}${split[1]}\n`;
       results += `${elem1}${obj[i]}${elem2}\n`;
     }
   } catch (err) {
@@ -39,10 +21,5 @@ const loop = (elem1, obj, elem2) => {
   }
   return results;
 };
-
-//loop('<li>Number <<>></li>\n', [6, 7, 8])
-//loop('<li>Number ', [6, 7, 8], '</li>')
-
-//console.timeEnd('benchmark');
 
 module.exports = loop;
